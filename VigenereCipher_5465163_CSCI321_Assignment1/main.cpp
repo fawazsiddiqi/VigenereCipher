ebe3b85bc8f75f41eb73711d02ec5612f02b3b5d
Fawaz Siddiqi <mfawazsiddiqi@gmail.com>
2019-03-18 21:56:35 +0400

    Code Formatted "Comments to be added"

--- VigenereCipher_5465163_CSCI321_Assignment1/main.cpp
+++ VigenereCipher_5465163_CSCI321_Assignment1/main.cpp
@@ -20,7 +20,7 @@ using namespace std;
 bitset<4> hex_char_to_bin(char c);
 vector<bitset<8>> read_file(string filename,int &countCipherText);
 void findCommon(vector<pair<int,int>>&eLocations ,vector<pair<int,int>>&spaceLocations,vector<vector<bitset<8>>> extracted);
-vector<vector<bitset<8>>> extract(int size,vector<bitset<8>> characters);
+vector<vector<bitset<8>>> segregateCiphertoKey(int size,vector<bitset<8>> characters);
 int findFrequency(bitset<8> character,vector<bitset<8>>);
 int sum(int size,vector<bitset<8>> characters,vector<vector<bitset<8>>> extractedCharacters);
 double calculateIC(int F,int N);
@@ -48,19 +48,22 @@ int main()
     vector<bitset<8>> KEY;
     //*******************************************************************************************
     start = clock();
-    characters=read_file("new-extended-given-ciphertext.txt",countCipherText); //reading the file and storing the ciphertext into a vector
-    
+    characters=read_file("new-extended-given-ciphertext.txt",countCipherText); //reading the cipher file and storing the ciphertext into a vector and getting number of characters
+    cout << "********************* STARTING PROGRAM *********************" <<endl;
+    cout << "********************** IC CALCULATION **********************" <<endl;
     for(int size=1;size<characters.size() && size<101;size++){ //Calculating and displaying IC for each and every keysize
         extractedCharacters.clear();
-        extractedCharacters=extract(size, characters);
-          
+        extractedCharacters=segregateCiphertoKey(size, characters);
         cout<<"IC: "<<calculateIC(sum(size,characters,extractedCharacters), (int)characters.size())<<" For key size: "<<size<<endl;
            
     }
-    cout<<"Enter key size\n";
+    cout <<endl;
+    cout<<"Please enter the key size with the first spike in the IC value: ";
     cin>>keySize;  //user enters most likely keysize
     
-    extractedCharacters=extract(keySize,characters); //the extracted characters are then extracted into columns which correspond to the poition according to the keysize
+    cout << endl;
+    
+    extractedCharacters=segregateCiphertoKey(keySize,characters); //the extracted characters are then extracted into columns which correspond to the poition according to the keysize
   
     //*******************************************************************************************
     //INITIALIZING PLAINTEXT AND KEY
@@ -78,7 +81,10 @@ int main()
     //*******************************************************************************************
     //FINDING SPACES
     //*******************************************************************************************
+    cout << "********* FINDING SPACES AND E LOCATIONS IN THE CIPHER TEXT *********" <<endl;
     findCommon(eLocations,spaceLocations, extractedCharacters);
+    
+    cout <<endl;
     
     for(int i=0;i<spaceLocations.size();i++){
         int f=spaceLocations.at(i).first;
@@ -86,9 +92,13 @@ int main()
         plaintext.at(f).at(s)=bitset<8>(32);
         KEY.at(f)=bitset<8>(32)^extractedCharacters.at(f).at(s); // finding the key
     }
-    //*******************************************************************************************
     
-    cout<<"KEY: "; //Getting the key
+    
+    //*******************************************************************************************
+    //FINDING THE KEY AND THE PLAINTEXT
+    //*******************************************************************************************
+    cout << "****************** GETTING INITIAL KEY ******************" <<endl;
+    cout<<"INITAL KEY: "; //Getting the key
     for(int i=0;i<KEY.size();i++){
         if(i==0)
         {
@@ -102,8 +112,9 @@ int main()
             cout<<""<<(char)(KEY.at(i).to_ulong())<<"";
         }
    }
-
-    findPlainText(KEY, plaintext, extractedCharacters); //finding the plaintext
+    cout <<endl;
+    cout << "******************* INITIAL PLAINTEXT *******************" <<endl;
+    findPlainText(KEY, plaintext, extractedCharacters); //finding the initial plaintext
    
     for(int i=0;i<plaintext.at(0).size();i++){ //displaying the plaintext
         for(int j=0;j<KEY.size();j++)
@@ -115,19 +126,23 @@ int main()
             }
         }
     }
-    cout<<endl;
-    
-    
+    cout<<endl<<endl;;
+   
     string partOfText="In Ethereum, smart contracts are written in the "; //the first 48 characters of the plaintext  and guessing the right letters
-        
-
-        for(int i=0;i<1;i++){ //Getting the key for the guess plaintext
+    cout << "The guess of the first 48 characters of the plaintext:" <<endl;
+    cout<< partOfText <<endl;
+    
+    cout << endl;
+    cout << "**************** GETTING FINAL KEY FROM GUESS ****************" <<endl;
+        for(int i=0;i<1;i++){ //Getting the key for the guess plaintext (final key)
             for(int j=0;j<KEY.size();j++)
             {
                 KEY.at(j)=bitset<8>((int)(partOfText.at(j)))^extractedCharacters.at(j).at(i);
             }
         }
-    cout<<"KEY: ";
+    cout <<endl;
+    
+    cout<<"FINAL KEY: ";
     for(int i=0;i<KEY.size();i++){
         if(i==0)
         {
@@ -141,7 +156,8 @@ int main()
             cout<<""<<(char)(KEY.at(i).to_ulong())<<"";
         }
     }
-    
+    cout << endl;
+    cout << "******************* FINAL PLAINTEXT *******************" <<endl <<endl;
         findPlainText(KEY, plaintext, extractedCharacters); //finding the new final plaintext
         countPlainText=0;
         for(int i=0;i<plaintext.at(0).size();i++){
@@ -154,11 +170,16 @@ int main()
                 }
             }
         }
+    cout << endl;
+    //*******************************************************************************************
         cout<<endl;
-    duration = (clock() - start ) / (double) CLOCKS_PER_SEC;
-    cout << "Code executed in " << duration << "s" <<endl;
-    return 0;
+        duration = (clock() - start ) / (double) CLOCKS_PER_SEC;
+        cout << "Code executed in " << duration << "s" <<endl;
+        return 0;
 }
+
+//*******************************************************************************************
+//*******************************************************************************************
 
 bitset<4> hex_char_to_bin(char c){
     // TODO handle default / error
@@ -192,16 +213,18 @@ vector<bitset<8>> read_file(string filename,int &countCipherText){
     vector<bitset<8>> characters;
     file.open(filename,ios::in);
     int count=0;
-    if(file.fail()){
+    if(file.fail())
+    {
         cout<<"File not found"<<endl;
-    }else{
+    }
+    else
+    {
         while(file>>character1){
             file>>character2;
             result=hex_char_to_bin(character1).to_string()+hex_char_to_bin(character2).to_string();
             characters.push_back(bitset<8>(result));
             count++;
         }
-        cout<<"COUNT: "<<count<<endl;
         countCipherText=count;
         file.close();
     }
@@ -237,7 +260,7 @@ void findCommon(vector<pair<int,int>>&eLocations ,vector<pair<int,int>>&spaceLoc
     }
 }
 
-vector<vector<bitset<8>>> extract(int size,vector<bitset<8>> characters){
+vector<vector<bitset<8>>> segregateCiphertoKey(int size,vector<bitset<8>> characters){
     vector<vector<bitset<8>>> temp;
     int pos=0;
     for(int i=0;i<size;i++){
